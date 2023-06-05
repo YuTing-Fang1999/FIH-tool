973a276cd7e1ec4d457b0485ae7d3b322172086a
yuting <s830s200s@gmail.com>
2023-06-05 16:31:12 +0800

    將xlwings移除，將excel設為唯獨

--- myPackage/OpenExcelBtn.py
+++ myPackage/OpenExcelBtn.py
@@ -4,22 +4,28 @@ from PyQt5.QtWidgets import (
     QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QTableWidget, QTableWidgetItem,
     QScrollArea, QFileDialog, QHBoxLayout, QSpacerItem, QSizePolicy
 )
-import xlwings as xw
+import win32com.client as win32
+
 class OpenExcelBtn(QPushButton):
     def __init__(self, text, fname):
         super().__init__(text)
         self.clicked.connect(lambda: self.open_excel(fname))
 
     def open_excel(self, fname):
-        app = xw.App(visible=True)
-        app.books[0].close()
-        
-        # Maximize the Excel window
-        app.api.WindowState = xw.constants.WindowState.xlMaximized
-        wb = app.books.open(fname)
-        # Set the Excel window as the foreground window
-        wb.app.activate(steal_focus=True)
+        # Open Excel application
+        excel = win32.Dispatch("Excel.Application")
 
+        # Open the Excel file in read-only mode
+        workbook = excel.Workbooks.Open(fname, ReadOnly=True)
+
+        # Set Excel window as the foreground window
+        excel.Visible = True
+        excel.WindowState = win32.constants.xlMaximized
+        
+        # Set the Excel window as the foreground window
+        workbook.Activate()
+
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     # 創建窗口
