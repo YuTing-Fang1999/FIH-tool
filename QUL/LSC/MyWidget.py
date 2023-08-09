734b87ea423db994c627b4f6e1a4149c14362eca
TrevorEChen <TrevorEChen@fih-foxconn.com>
2023-08-09 09:32:53 +0800

    Modify LSC

--- QUL/LSC/MyWidget.py
+++ QUL/LSC/MyWidget.py
@@ -28,6 +28,88 @@ class ExcelWorkerThread(QThread):
             wb = openpyxl.load_workbook(fn, read_only=False, keep_vba=True)
             wb.active = 0
             return wb
+        
+        def find_lsc_data(self, root, lsc_type, aec_exp_control):
+            data = []
+            golden_data = []
+            
+            lsc_golden_rgn_data = root.find(f'.//{lsc_type}_golden_rgn_data')
+            r_gain_values = lsc_golden_rgn_data.find('r_gain_tab/r_gain').text
+            r_gain_list = [float(value) for value in r_gain_values.split()]
+            gr_gain_values = lsc_golden_rgn_data.find('gr_gain_tab/gr_gain').text
+            gr_gain_list = [float(value) for value in gr_gain_values.split()]
+            gb_gain_values = lsc_golden_rgn_data.find('gb_gain_tab/gb_gain').text
+            gb_gain_list = [float(value) for value in gb_gain_values.split()]
+            b_gain_values = lsc_golden_rgn_data.find('b_gain_tab/b_gain').text
+            b_gain_list = [float(value) for value in b_gain_values.split()]
+            
+            golden_data = {
+                "r_gain": r_gain_list,
+                "gr_gain": gr_gain_list,
+                "gb_gain": gb_gain_list,
+                "b_gain": b_gain_list
+            }
+            
+            if aec_exp_control == "control_lux_idx":
+                for i, mod_lsc_aec_data in enumerate(root.findall(f'.//mod_{lsc_type}_aec_data')):
+                    aec_trigger = mod_lsc_aec_data.find('aec_trigger')
+                    start = aec_trigger.find('lux_idx_start').text
+                    end = aec_trigger.find('lux_idx_end').text
+                
+                    for mod_lsc_cct_data in mod_lsc_aec_data.findall(f'.//mod_{lsc_type}_cct_data'):
+                        cct_trigger = mod_lsc_cct_data.find('cct_trigger')
+                        cct_start = cct_trigger.find('start').text
+                        cct_end = cct_trigger.find('end').text
+                        
+                        lsc_rgn_data = mod_lsc_cct_data.find(f'{lsc_type}_rgn_data')
+                        r_gain = lsc_rgn_data.find('r_gain_tab/r_gain').text
+                        gr_gain = lsc_rgn_data.find('gr_gain_tab/gr_gain').text
+                        gb_gain = lsc_rgn_data.find('gb_gain_tab/gb_gain').text
+                        b_gain = lsc_rgn_data.find('b_gain_tab/b_gain').text
+                
+                        data.append({
+                            "lux_idx_start": start,
+                            "lux_idx_end": end,
+                            "cct_data": {
+                                "start": cct_start,
+                                "end": cct_end,
+                                "r_gain": r_gain,
+                                "gr_gain": gr_gain,
+                                "gb_gain": gb_gain,
+                                "b_gain": b_gain,
+                            }
+                        })
+            else:
+                for i, mod_lsc_aec_data in enumerate(root.findall(f'.//mod_{lsc_type}_aec_data')):
+                    aec_trigger = mod_lsc_aec_data.find('aec_trigger')
+                    start = aec_trigger.find('gain_start').text
+                    end = aec_trigger.find('gain_end').text
+                
+                    for mod_lsc_cct_data in mod_lsc_aec_data.findall(f'.//mod_{lsc_type}_cct_data'):
+                        cct_trigger = mod_lsc_cct_data.find('cct_trigger')
+                        cct_start = cct_trigger.find('start').text
+                        cct_end = cct_trigger.find('end').text
+                        
+                        lsc_rgn_data = mod_lsc_cct_data.find(f'{lsc_type}_rgn_data')
+                        r_gain = lsc_rgn_data.find('r_gain_tab/r_gain').text
+                        gr_gain = lsc_rgn_data.find('gr_gain_tab/gr_gain').text
+                        gb_gain = lsc_rgn_data.find('gb_gain_tab/gb_gain').text
+                        b_gain = lsc_rgn_data.find('b_gain_tab/b_gain').text
+                
+                        data.append({
+                            "gain_start": start,
+                            "gain_end": end,
+                            "cct_data": {
+                                "start": cct_start,
+                                "end": cct_end,
+                                "r_gain": r_gain,
+                                "gr_gain": gr_gain,
+                                "gb_gain": gb_gain,
+                                "b_gain": b_gain,
+                            }
+                        })
+                    
+            return golden_data, data
 
         def run(self):
             print(f"Selected file: {self.filepath}")
@@ -37,54 +119,16 @@ class ExcelWorkerThread(QThread):
             tree = ET.parse(self.filepath)
             root = tree.getroot()
 
-            data = []
-            golden_data = []
+            if 'lsc34' in root.tag:
+                lsc_type = 'lsc34'
+            elif 'lsc35' in root.tag:
+                lsc_type = 'lsc35'
+            else:
+                raise ValueError('Unsupported LSC type in XML')
 
-            lsc34_golden_rgn_data = root.find('.//lsc34_golden_rgn_data')
-            r_gain_values = lsc34_golden_rgn_data.find('r_gain_tab/r_gain').text
-            r_gain_list = [float(value) for value in r_gain_values.split()]
-            gr_gain_values = lsc34_golden_rgn_data.find('gr_gain_tab/gr_gain').text
-            gr_gain_list = [float(value) for value in gr_gain_values.split()]
-            gb_gain_values = lsc34_golden_rgn_data.find('gb_gain_tab/gb_gain').text
-            gb_gain_list = [float(value) for value in gb_gain_values.split()]
-            b_gain_values = lsc34_golden_rgn_data.find('b_gain_tab/b_gain').text
-            b_gain_list = [float(value) for value in b_gain_values.split()]
-
-            golden_data = {
-                "r_gain": r_gain_list,
-                "gr_gain": gr_gain_list,
-                "gb_gain": gb_gain_list,
-                "b_gain": b_gain_list
-            }
-
-            for i, mod_lsc34_aec_data in enumerate(root.findall('.//mod_lsc34_aec_data')):
-                aec_trigger = mod_lsc34_aec_data.find('aec_trigger')
-                lux_idx_start = aec_trigger.find('lux_idx_start').text
-                lux_idx_end = aec_trigger.find('lux_idx_end').text
-
-                for mod_lsc34_cct_data in mod_lsc34_aec_data.findall('.//mod_lsc34_cct_data'):
-                    cct_trigger = mod_lsc34_cct_data.find('cct_trigger')
-                    start = cct_trigger.find('start').text
-                    end = cct_trigger.find('end').text
-                    
-                    lsc34_rgn_data = mod_lsc34_cct_data.find('lsc34_rgn_data')
-                    r_gain = lsc34_rgn_data.find('r_gain_tab/r_gain').text
-                    gr_gain = lsc34_rgn_data.find('gr_gain_tab/gr_gain').text
-                    gb_gain = lsc34_rgn_data.find('gb_gain_tab/gb_gain').text
-                    b_gain = lsc34_rgn_data.find('b_gain_tab/b_gain').text
-
-                    data.append({
-                        "lux_idx_start": lux_idx_start,
-                        "lux_idx_end": lux_idx_end,
-                        "cct_data": {
-                            "start": start,
-                            "end": end,
-                            "r_gain": r_gain,
-                            "gr_gain": gr_gain,
-                            "gb_gain": gb_gain,
-                            "b_gain": b_gain,
-                        }
-                    })
+            control_method = root.find('.//control_method')
+            aec_exp_control = control_method.find('aec_exp_control').text    
+            golden_data, data = self.find_lsc_data(root, lsc_type, aec_exp_control)
 
             localtime = time.localtime()
             clock = str(60*60*localtime[3] + 60*localtime[4] + localtime[5])
@@ -101,12 +145,20 @@ class ExcelWorkerThread(QThread):
             macro_vba = wb.app.macro('CopySheetWithChart')
 
             for i, item in enumerate(data, start=2):
-                print(f'region {str(i).rjust(3)}: '
-                    f'lux_idx_start: {str(item["lux_idx_start"]).rjust(5)}, '
-                    f'lux_idx_end: {str(item["lux_idx_end"]).rjust(5)}, '
-                    f'start: {str(item["cct_data"]["start"]).rjust(5)}, '
-                    f'end: {str(item["cct_data"]["end"]).rjust(5)}')
-                sheet_name = f'lux_{item["lux_idx_start"]}_{item["lux_idx_end"]}_cct_{item["cct_data"]["start"]}_{item["cct_data"]["end"]}'
+                if aec_exp_control == "control_lux_idx":
+                    print(f'region {str(i).rjust(3)}: '
+                          f'lux_idx_start: {str(item["lux_idx_start"]).rjust(5)}, '
+                          f'lux_idx_end: {str(item["lux_idx_end"]).rjust(5)}, '
+                          f'start: {str(item["cct_data"]["start"]).rjust(5)}, '
+                          f'end: {str(item["cct_data"]["end"]).rjust(5)}')
+                    sheet_name = f'lux_{item["lux_idx_start"]}_{item["lux_idx_end"]}_cct_{item["cct_data"]["start"]}_{item["cct_data"]["end"]}'
+                else:
+                    print(f'region {str(i).rjust(3)}: '
+                          f'gain_start: {str(item["gain_start"]).rjust(5)}, '
+                          f'gain_end: {str(item["gain_end"]).rjust(5)}, '
+                          f'start: {str(item["cct_data"]["start"]).rjust(5)}, '
+                          f'end: {str(item["cct_data"]["end"]).rjust(5)}')
+                    sheet_name = f'gain_{item["gain_start"]}_{item["gain_end"]}_cct_{item["cct_data"]["start"]}_{item["cct_data"]["end"]}'
                 macro_vba(sheet_name)
                 self.update_status_bar_signal.emit(sheet_name)
 
@@ -132,7 +184,6 @@ class ExcelWorkerThread(QThread):
                 for j, b_gain in enumerate(b_gain_values, start=3):
                     ws.cell(row=j, column=6).value = round(float(b_gain_values[j - 3]), 3)
 
-
             wb.active = 0
             ws = wb.active
             r_gain_values = golden_data["r_gain"]
@@ -157,9 +208,14 @@ class ExcelWorkerThread(QThread):
             self.is_processing = False
 
             item_list = []
-            for i, item in enumerate(data, start=2):
-                item_name = f'lux_{item["lux_idx_start"]}_{item["lux_idx_end"]}_cct_{item["cct_data"]["start"]}_{item["cct_data"]["end"]}'
-                item_list.append(item_name)
+            if aec_exp_control == "control_lux_idx":
+                for i, item in enumerate(data, start=2):
+                    item_name = f'lux_{item["lux_idx_start"]}_{item["lux_idx_end"]}_cct_{item["cct_data"]["start"]}_{item["cct_data"]["end"]}'
+                    item_list.append(item_name)
+            else:
+                for i, item in enumerate(data, start=2):
+                    item_name = f'lux_{item["gain_start"]}_{item["gain_end"]}_cct_{item["cct_data"]["start"]}_{item["cct_data"]["end"]}'
+                    item_list.append(item_name)
             item_list.append("LSC golden OTP(xml)")
             self.update_list_signal.emit(item_list)
 
