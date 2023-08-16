45dbc766972f2bdfb2eb243715c6cd012baed203
yuting <s830s200s@gmail.com>
2023-08-16 16:16:32 +0800

    修改AF說明

--- MTK/AF/Intro.py
+++ MTK/AF/Intro.py
@@ -5,11 +5,9 @@ class Intro(QTextBrowser):
     def __init__(self):
         super().__init__()
         markdown_content = """
-用來從log中擷取出每張照片帶有特定關鍵字的log資訊。  
-  
-適合使用的時機:需要連續拍攝很多張照片，並分析某幾張照片的AF對焦行為。例如: AF lab大量測試、外拍有拍攝大量照片  
-提示: DbgCfgTool alog或是MTK DebugLogger需紀錄到時間(e.g. DbgCfgTool apk要將Log Output Format設定成threadtime)，否則工具無法解析。  
-
+#### 請事先使用Qualcomm DbgCfgTool apk或是MTK DebugLogger apk軟體錄製log， 而且DbgCfgTool或是DebugLogger需要紀錄到時間(例如: DbgCfgTool apk要將Log Output Format設定成threadtime)，否則此工具無法解析。   
+此工具是用來從log中擷取出每張照片帶有特定關鍵字的log資訊。   
+適合使用的時機: 需要連續拍攝很多張照片，並分析某幾張照片的AF對焦行為。例如: AF lab大量測試、外拍有拍攝大量照片的情況。   
 """
         html = markdown.markdown(markdown_content)
         self.setHtml(html)
--- QUL/LSC/Intro.py
+++ QUL/LSC/Intro.py
@@ -9,14 +9,11 @@ class Intro(QTextBrowser):
 ### 目的:  
 使用LSC Golden sample 裝在手機上容易單邊傾斜, 觀察四個LSC Calibration Gain 是否對稱是預防單邊傾斜   
 確認邊緣亮度補償程度、觀察LSC補償過後圖形是否均勻且平整   
-<br>
 ### 操作:  
 1.點”Load and Export LSC golden OTP txt”按鈕選擇廠商給的golden OTP txt，並選擇轉置C7直行的形式的golden OTP txt的儲存位置，並去 C7 >> LSC >> golden module >> import 選txt，做各region 的 LSC 補償優化  
 2. 點"Load lsc .xml"按鈕選擇lsc.xml 檔案 load 入整合 tool  
 3. 點下拉式選單選擇想看的region資料  
 4. 點" Open LSC excel"按鈕如果想看更詳細的資料再開啟 excel 檢查平坦度  
-
-
 """
         html = markdown.markdown(markdown_content)
         self.setHtml(html)
