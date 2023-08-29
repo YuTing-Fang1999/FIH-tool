b0526beb8e1b859d17c8831dba3f09d9168c542f
yuting <s830s200s@gmail.com>
2023-08-29 09:21:55 +0800

    mtkAEclassify 統計圖未顯示 debug

--- MTK/AE/mtkAEclassify/MyWidget.py
+++ MTK/AE/mtkAEclassify/MyWidget.py
@@ -17,7 +17,7 @@ from MTK.AE.mtkAEclassify.mtkAEclassify_EVDB2M import EVDB2M
 
 import re
 
-class WorkerThread(QThread):
+class ClassifyThread(QThread):
     finish_signal = pyqtSignal() 
     exif_path = None
     def __init__(self):
@@ -27,29 +27,58 @@ class WorkerThread(QThread):
         B2Dmidratio(self.exif_path)
         EVDB2M(self.exif_path)
         self.finish_signal.emit()
-        
+
+class SetImgGridThread(QThread):
+    finish_signal = pyqtSignal(int)
+    failed_signal = pyqtSignal(str)
+    set_img_grid_signal = pyqtSignal(np.ndarray, int ,int) 
+    dir = None
+    img_num = 0
+    img_width = 220
+    classify_type = None
+    def __init__(self):
+        super().__init__()
+            
+    def run(self):
+        try:
+            self.img_num = 0
+            # 遞迴列出所有檔案的絕對路徑
+            for root, dirs, files in os.walk(self.dir):
+                if "small" not in root: continue
+                def atoi(text):
+                    return int(text) if text.isdigit() else text
+                def natural_keys(text):
+                    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+                files.sort(key=natural_keys)
+                for f in files:
+                    fullpath = os.path.join(root, f)
+                    if self.classify_type in fullpath:
+                        if (".jpg" in fullpath.lower() or ".jpeg" in fullpath.lower()) and "exif" not in fullpath.lower():
+                            # print(fullpath)
+                            img = cv2.imdecode( np.fromfile( file = fullpath, dtype = np.uint8 ), cv2.IMREAD_COLOR )
+                            cv2.putText(img, f.split("_")[0], (25, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                            self.set_img_grid_signal.emit(img, self.img_num//4, self.img_num%4)
+                            self.img_num +=1
+            
+            self.finish_signal.emit(self.img_num)
+        except Exception as error:
+            print(error)
+            self.failed_signal.emit("Failed\n"+str(error))
+            self.finish_signal.emit(0)
+            
 class MyWidget(ParentWidget):
     def __init__(self):
         super().__init__()  # in python3, super(Class, self).xxx = super().xxx
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-        self.worker = WorkerThread()
-        self.pre_dir = ""
-        self.pre_radio = None
+        self.classify_thread = ClassifyThread()
+        self.set_img_grid_thread = SetImgGridThread()
+        self.pre_filename = ""
+        self.now_radio = None
         
         self.setupUi()
         self.controller()
-    
-    def controller(self):
-        self.ui.load_exif_btn.clicked.connect(self.load_exif)
-        self.ui.load_after_dir_btn.clicked.connect(self.load_after_dir)
-        self.ui.open_dir_btn.clicked.connect(self.open_dir)
-        self.worker.finish_signal.connect(self.after_load_exif)
-        self.ui.button_group.buttonClicked.connect(self.set_BV_comboBox)
-        self.ui.BV_comboBox.currentTextChanged.connect(self.change_BV_comboBox)
-        self.ui.B2D_EVD_comboBox.currentTextChanged.connect(self.change_B2D_EVD_comboBox)
-        self.ui.Mid_B2M_comboBox.currentTextChanged.connect(self.change_Mid_B2M_comboBox)
-    
+        
     def setupUi(self):
         self.chart_viewer = ImageViewer()
         self.ui.horizontalLayout_4.addWidget(self.chart_viewer)
@@ -61,22 +90,30 @@ class MyWidget(ParentWidget):
         self.ui.B2D_EVD_comboBox.setEnabled(False)
         self.ui.Mid_B2M_comboBox.setEnabled(False)
         
-        # self.load_exif()
+    def controller(self):
+        self.ui.load_exif_btn.clicked.connect(self.load_exif)
+        self.ui.load_after_dir_btn.clicked.connect(self.load_after_dir)
+        self.ui.open_dir_btn.clicked.connect(self.open_dir)
         
-    def set_btn_enable(self, btn: QPushButton, enable):
-        if enable:
-            style =  "QPushButton {background:rgb(68, 114, 196); color: white;}"
-        else:
-            style =  "QPushButton {background: rgb(150, 150, 150); color: rgb(100, 100, 100);}"
-        btn.setStyleSheet(style)
-        btn.setEnabled(enable)
+        self.ui.button_group.buttonClicked.connect(self.set_BV_comboBox)
+        self.ui.BV_comboBox.currentTextChanged.connect(self.change_BV_comboBox)
+        self.ui.B2D_EVD_comboBox.currentTextChanged.connect(self.change_B2D_EVD_comboBox)
+        self.ui.Mid_B2M_comboBox.currentTextChanged.connect(self.change_Mid_B2M_comboBox)
+        
+        self.classify_thread.finish_signal.connect(self.after_load_exif)
+        self.set_img_grid_thread.finish_signal.connect(self.after_set_img_grid)
+        self.set_img_grid_thread.set_img_grid_signal.connect(self.set_img_grid)
+        
+    def failed(self, text="Failed"):
+        self.set_all_enable(True)
+        QMessageBox.about(self, "Failed", text)
         
     def load_exif(self):
         filepath = QFileDialog.getExistingDirectory(self,"選擇Exif資料夾", self.get_path("MTK_AE_mtkAEclassify_exif"))
 
         if filepath == '':
             return
-        self.worker.exif_path = filepath
+        self.classify_thread.exif_path = filepath
         filefolder = '/'.join(filepath.split('/')[:-1])
         self.set_path("MTK_AE_mtkAEclassify_exif", filefolder)
         self.set_all_enable(False, "載入中，請稍後...")
@@ -87,17 +124,15 @@ class MyWidget(ParentWidget):
         self.ui.weighting_radio.setChecked(False)
         self.ui.button_group.setExclusive(True)
         
-        self.worker.start()
+        self.classify_thread.start()
         
     def load_after_dir(self):
-        # filepath = "C:/Users/s830s/OneDrive/文件/github/FIH-tool整合/說明/4.mtkAEclassify/all"
-        
         filepath = QFileDialog.getExistingDirectory(self,"選擇已經分類過的資料夾", self.get_path("MTK_AE_mtkAEclassify_after"))
 
         if filepath == '':
             return
         
-        self.worker.exif_path = filepath
+        self.classify_thread.exif_path = filepath
         filefolder = '/'.join(filepath.split('/')[:-1])
         self.set_path("MTK_AE_mtkAEclassify_after", filefolder)
         self.set_all_enable(False, "載入中，請稍後...")
@@ -106,9 +141,9 @@ class MyWidget(ParentWidget):
     def after_load_exif(self):
         self.weighting_dir = {}
         self.THD_dir = {}
-        for file in os.listdir(self.worker.exif_path):
-            if "BV" in file and os.path.isdir(self.worker.exif_path + "/" + file):
-                path = self.worker.exif_path + "/" + file
+        for file in os.listdir(self.classify_thread.exif_path):
+            if "BV" in file and os.path.isdir(self.classify_thread.exif_path + "/" + file):
+                path = self.classify_thread.exif_path + "/" + file
                 if os.path.isdir(path):
                     for file2 in os.listdir(path):
                         if "B2D" in file2 and os.path.isdir(path + "/" + file2):
@@ -130,8 +165,6 @@ class MyWidget(ParentWidget):
                                 if os.path.isdir(path + "/" + file2 + "/" + file3):
                                     self.THD_dir[file][file2].append(file3)
                     
-        # print(self.weighting_dir)
-        # print(self.THD_dir)
         self.set_btn_enable(self.ui.load_exif_btn, True)
         self.set_btn_enable(self.ui.load_after_dir_btn, True)
         self.ui.load_exif_btn.setText("選擇照片、exif 資料夾\n並執行分類")
@@ -201,11 +234,10 @@ class MyWidget(ParentWidget):
         self.ui.B2D_EVD_comboBox.setEnabled(state)
         self.ui.Mid_B2M_comboBox.setEnabled(state)
         self.ui.load_exif_btn.setText(text)
-        self.ui.load_exif_btn.repaint()
         
     def set_graph(self):
                 
-        dir = self.worker.exif_path + "/"
+        dir = self.classify_thread.exif_path + "/"
         if self.now_radio == self.ui.weighting_radio:
             filename = "B2D_"
         elif self.now_radio == self.ui.THD_radio:
@@ -222,12 +254,13 @@ class MyWidget(ParentWidget):
         if self.ui.Mid_B2M_comboBox.currentText() != "" and self.ui.Mid_B2M_comboBox.currentText() != "all": 
             dir += self.ui.Mid_B2M_comboBox.currentText() + "/"
             filename += "_"+self.ui.Mid_B2M_comboBox.currentText()
-            
-        if (self.pre_dir == dir or dir == self.worker.exif_path + "/") and self.pre_radio == self.now_radio: return
-        self.pre_dir = dir
-        self.pre_radio = self.now_radio
-        filename += ".png"
         
+        filename += ".png"
+    
+        if self.pre_filename == filename: return
+        self.pre_filename = filename
+        self.pre_dir = dir
+
         self.set_all_enable(False, "載入中，請稍後...")
         
         # deleteAllItems
@@ -235,41 +268,28 @@ class MyWidget(ParentWidget):
             widget = self.ui.photo_grid.itemAt(i).widget()
             if widget is not None:
                 widget.deleteLater()
-        print(dir)
-        i = 0
-        img_width = 220
-        # 遞迴列出所有檔案的絕對路徑
-        for root, dirs, files in os.walk(dir):
-            if "small" not in root: continue
-            def atoi(text):
-                return int(text) if text.isdigit() else text
-            def natural_keys(text):
-                return [ atoi(c) for c in re.split(r'(\d+)', text) ]
-            files.sort(key=natural_keys)
-            for f in files:
-                fullpath = os.path.join(root, f)
-                if (self.ui.weighting_radio.isChecked() and "B2D" in fullpath) or (self.ui.THD_radio.isChecked() and "EVD" in fullpath):
-                    if (".jpg" in fullpath.lower() or ".jpeg" in fullpath.lower()) and "exif" not in fullpath.lower():
-                        # print(fullpath)
-                        img = cv2.imdecode( np.fromfile( file = fullpath, dtype = np.uint8 ), cv2.IMREAD_COLOR )
-                        # width = 200
-                        # height = int(width * img.shape[0] / img.shape[1])
-                        # img = cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
-                        cv2.putText(img, f.split("_")[0], (25, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-                        viewer = ImageViewer()
-                        viewer.setMinimumWidth(img_width)
-                        viewer.setMinimumHeight(img_width)
-                        viewer.setMaximumWidth(img_width)
-                        viewer.setMaximumHeight(img_width)
-                        viewer.wheelEvent = lambda event: None
-                        viewer.setPhoto(img)
-                        self.ui.photo_grid.addWidget(viewer, i//4, i%4)
-                        i+=1
-        if i<=4:
-            self.ui.photo_scrollArea.setMinimumWidth((img_width+5)*i)
+                
+        self.set_img_grid_thread.dir = dir
+        self.set_img_grid_thread.classify_type = filename.split("_")[0]
+        self.set_img_grid_thread.start()
+        
+    def set_img_grid(self, img, row, col):
+        self.img_width = 220
+        viewer = ImageViewer()
+        viewer.setMinimumWidth(self.img_width)
+        viewer.setMinimumHeight(self.img_width)
+        viewer.setMaximumWidth(self.img_width)
+        viewer.setMaximumHeight(self.img_width)
+        viewer.wheelEvent = lambda event: None
+        viewer.setPhoto(img)
+        self.ui.photo_grid.addWidget(viewer, row, col)
+        
+    def after_set_img_grid(self, img_num):
+        if img_num<=4:
+            self.ui.photo_scrollArea.setMinimumWidth((self.set_img_grid_thread.img_width+5)*img_num)
         else: 
-            self.ui.photo_scrollArea.setMinimumWidth((img_width+5)*4)
-        img = cv2.imdecode( np.fromfile( file = dir + "/" + filename, dtype = np.uint8 ), cv2.IMREAD_COLOR )
+            self.ui.photo_scrollArea.setMinimumWidth((self.set_img_grid_thread.img_width+5)*4)
+        img = cv2.imdecode( np.fromfile( file = self.pre_dir + "/" + self.pre_filename, dtype = np.uint8 ), cv2.IMREAD_COLOR )
         self.chart_viewer.setPhoto(img)
         self.set_all_enable(True, "選擇照片、exif 資料夾\n並執行分類")
         
